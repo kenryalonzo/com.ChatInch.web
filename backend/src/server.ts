14f44df02526aaf32be9f58ac4c5646d617ab2f3
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-04-13 11:49:21 +0100

    feat: backend config

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import authRoutes from './routes/auth.route';
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5001, () => {
+  console.log('Server is running on port 5001');
+}
+);
