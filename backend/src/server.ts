62fbf9432eec46a82c8143633cf5ffbaac9ebbec
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-04-14 09:02:36 +0100

    fix: continue without docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { connectDB } from './lib/db';
+
 import authRoutes from './routes/auth.route';
 
 dotenv.config();
@@ -12,5 +14,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}
-);
+  connectDB();
+});
