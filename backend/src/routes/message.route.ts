747e2fde4a1673667396b8f6277ed5c1a2ce0a00
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-04-16 06:05:34 +0100

    fix: ajustement des routes

---
+++ backend/src/routes/message.route.ts
@@ -0,0 +1,13 @@
+import { protectRoute } from "@/middleware/auth.midlleware";
+import express from "express";
+
+import { getMessages, getUsersForSidebar, sendMessage } from "@/controllers/message.controller";
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
